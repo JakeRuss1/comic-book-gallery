128aa76097c85e9aeb56155f6fcec49368ed36a7
Your Name <you@example.com>
2018-11-08 19:09:28 -0500

    big updates

---
+++ comic-book-gallery/Models/ComicBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace comic_book_gallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public string IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayTextc
+        {
+            get
+            {
+                return SeriesTitle + "#" + IssueNumber;
+
+            }
+        }
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace("", "-")
+                 .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+    }
+
+
+}
+}
